01ff39f0f47126cb89edb0dc1536bc54a88d750e
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-02-02 02:33:46 -0300

    Criando backend em Node com Express. Configurando base com Knex.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express, { response } from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    return response.json(['Hello World', 'Test'])
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
