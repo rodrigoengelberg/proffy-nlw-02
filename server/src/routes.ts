3a9f8af05bc33bdb1c9c96d5f13beb37e66bbf57
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-02-02 23:04:23 -0300

    Criação dos insertes de Classes, ClassSchedule e Users

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,14 @@
 import express from 'express'
 import db from './database/connection'
+import convertHourToMinutes from './utils/convertHourToMinutes'
 
 const routes = express.Router()
+
+interface ScheduleItem {
+    week_day: number
+    from: string
+    to: string
+}
 
 routes.post('/classes', async (request, response) => {
     
@@ -15,22 +22,47 @@ routes.post('/classes', async (request, response) => {
         schedule
     } = request.body
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio
-    })
+    const trx = await db.transaction()
 
-    const user_id = insertedUsersIds[0];
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio
+        })
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesId = await trx('classes').insert({
+            subject,
+            cost,
+            user_id
+        })
+    
+        const class_id = insertedClassesId[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to)
+            }
+        })
+    
+        await trx('class_schedule').insert(classSchedule)
 
-    await db('classes').insert({
-        subject,
-        cost,
-        user_id
-    })
+        await trx.commit()
+    
+        return response.status(201).send()
+    } catch(err) {
+        await trx.rollback()
 
-    return response.send()
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class'
+        })
+    }
 })
 
 export default routes
